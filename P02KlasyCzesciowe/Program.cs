3adab18719c2c2753912b60edb6c1dd6f0e91983
kurs <kurs@DESKTOP-284UO99>
2024-11-05 09:56:00 +0100

    q

---
+++ P02KlasyCzesciowe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02KlasyCzesciowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zawodnik z = new Zawodnik();
+            z.Imie = "Jan";
+            z.Nazwisko = "Kowalski";
+
+
+        }
+    }
+}
